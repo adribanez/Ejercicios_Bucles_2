d753a9032f637ad32087b882e1323c7ef592a204
Adrián Ibáñez <120591858+adribanez@users.noreply.github.com>
2023-03-27 18:33:41 +0200

    Add files via upload

---
+++ Ejercicio_Switch.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Elija un programa de los siguientes: A. C#    B. Java    C. C++");
+String programa = Console.ReadLine();
+
+switch (programa)
+{
+    case "A":
+        Console.WriteLine("Usted ha elegido el programa C#");
+        break;
+
+    case "B":
+        Console.WriteLine("Usted ha elegido el programa Java");
+        break;
+
+    case "C":
+        Console.WriteLine("Usted ha elegido el programa C++");
+        break;
+
+    default:
+        Console.WriteLine("No conozco ese dato");
+        break;
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("¿Cual es su nombre?");
+
+
+
+Console.WriteLine("Encantado " + Console.ReadLine());
+
+
+
+Console.WriteLine("Introduce tu email");
+
+
+
+Console.WriteLine("su email es : " + Console.ReadLine());
+
+
+
+Console.WriteLine("¿tiene cupon de descuento?");
+
+
+String cupon = Console.ReadLine();
+
+if (cupon =="si")
+{
+    Console.WriteLine("El precio final de su producto por el cupón descuento sera de 180 Euros");
+}
+
+ else
+{
+    Console.WriteLine("El precio final de su producto sera de 200 Euros");
+}
+    
+
+    
+
+
+
